f47417486f9aec804a923932ea53d232733b1932
Pedro Pereira <pedronprpereira@gmail.com>
2024-04-26 12:06:14 +0200

    Added Default Connection String

--- TimeTracker.Api/GlobalUsings.cs
+++ TimeTracker.Api/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using TimeTracker.Shared.Entities;
 global using TimeTracker.Api.Repositories;
 global using TimeTracker.Api.Services;
 global using TimeTracker.Shared.Models.TimeEntry;
+global using TimeTracker.Api.Data;
