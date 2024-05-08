b788a1915522dc4350dd8f0275da3ec2cbcad74b
Pedro Pereira <pedronprpereira@gmail.com>
2024-05-08 11:24:34 +0200

    Create Project entity

---
+++ TimeTracker.Shared/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeTracker.Shared.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime? DateUpdated { get; set; }
+    }
+}
---
+++ TimeTracker.Shared/Entities/SoftDeletableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeTracker.Shared.Entities
+{
+    public class SoftDeletableEntity : BaseEntity
+    {
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DateDeleted { get; set; }
+    }
+}
