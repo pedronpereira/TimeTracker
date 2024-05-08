06fcdf17dfdd3c03d07bf95f2716fdda5cbb887b
Pedro Pereira <pedronprpereira@gmail.com>
2024-05-08 10:37:24 +0200

    Add InitialCreate database script

---
+++ TimeTracker.Api/Scripts/InitialCreate.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [TimeEntries] (
+    [Id] nvarchar(450) NOT NULL,
+    [Project] nvarchar(max) NOT NULL,
+    [Start] datetime2 NOT NULL,
+    [End] datetime2 NULL,
+    [DateCreated] datetime2 NOT NULL,
+    [DateUpdated] datetime2 NOT NULL,
+    CONSTRAINT [PK_TimeEntries] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240426101642_InitialCreate', N'8.0.4');
+GO
+
+COMMIT;
+GO
+
