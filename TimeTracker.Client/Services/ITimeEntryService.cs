6de43b4d0584cfc44d461169f07cf92ca257044a
Pedro Pereira <pedronprpereira@gmail.com>
2024-05-21 22:19:15 +0200

    Added ProjectSelector and Navigation for EditTimeEntry

---
+++ TimeTracker.Client/Services/ITimeEntryService.cs
@@ -0,0 +1,14 @@
+﻿using TimeTracker.Shared.Models.TimeEntry;
+
+namespace TimeTracker.Client.Services
+{
+    public interface ITimeEntryService
+    {
+        event Action? OnChange;
+
+        public List<TimeEntryResponse> TimeEntries { get; set; }
+        Task GetTimeEntryiesByProject(string projectId);
+        Task<TimeEntryResponse> GetTimeEntryById(string projectId);
+
+    }
+}
---
+++ TimeTracker.Client/Services/TimeEntryService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using TimeTracker.Shared.Models.TimeEntry;
+
+namespace TimeTracker.Client.Services
+{
+    public class TimeEntryService : ITimeEntryService
+    {
+        private readonly HttpClient _http;
+        public List<TimeEntryResponse> TimeEntries { get; set; } = new List<TimeEntryResponse>();
+
+        public event Action? OnChange;
+
+        public TimeEntryService(HttpClient httpClient)
+        {
+            _http = httpClient;
+        }
+        public async Task GetTimeEntryiesByProject(string projectId)
+        {
+            List<TimeEntryResponse>? result = null;
+            if(string.IsNullOrWhiteSpace(projectId))
+            {
+                result = await _http.GetFromJsonAsync<List<TimeEntryResponse>>("api/timeentry");
+            }
+        else
+            {
+                result = await _http.GetFromJsonAsync<List<TimeEntryResponse>>($"api/timeentry/project/{projectId}");
+            }
+
+            if (result != null)
+            {
+                TimeEntries = result;
+                OnChange?.Invoke();
+            }
+        }
+
+        public async Task<TimeEntryResponse> GetTimeEntryById(string projectId)
+        {
+            return await _http.GetFromJsonAsync<TimeEntryResponse>($"api/timeentry/{projectId}");
+        }
+    }
+}
